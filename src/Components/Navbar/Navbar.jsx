fc1cbfa434ba2c21a14eb82f12d82b72a38497c0
kartik <agarwalkartik704@gmail.com>
2025-04-27 22:26:58 +0530

    first commit

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import "./Navbar.css";
+import menu_icon from "../../assets/menu.png";
+import logo from "../../assets/logo.png";
+import search_icon from "../../assets/search.png";
+import upload_icon from "../../assets/upload.png";
+import more_icon from "../../assets/more.png";
+import notification_icon from "../../assets/notification.png";
+import profile_icon from "../../assets/jack.png";
+
+const Navbar = ({ setSidebar }) => {
+  return (
+    <nav className="flex-div">
+      <div className="nav-left flex-div">
+        <img
+          className="menu-icon"
+          onClick={() => {
+            setSidebar((prev) => !prev);
+          }}
+          src={menu_icon}
+          alt="Menu"
+        />
+        <img className="logo" src={logo} alt="Logo" />
+      </div>
+
+      <div className="nav-middle flex-div">
+        <div className="search-box flex-div">
+          <input type="text" placeholder="Search" />
+          <img src={search_icon} alt="Search" />
+        </div>
+      </div>
+
+      <div className="nav-right flex-div">
+        <img src={upload_icon} alt="Upload" />
+        <img src={more_icon} alt="More Options" />
+        <img src={notification_icon} alt="Notifications" />
+        <img src={profile_icon} className="user-icon" alt="Profile" />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/data.js
@@ -0,0 +1,1 @@
+export const API_KEY = 'AIzaSyCaT7i2JRo9EU0tiZXoXFlW-LJ-N9k2HQU'
